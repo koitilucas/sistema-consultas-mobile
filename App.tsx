02b1146462225a47431da9cb721557bb168b928b
Lucas Koiti <lucas_koiti207@hotmail.com>
2026-09-18 20:16:48 -0300

    feat: tela Home e ConsultaCard com estilos separados - Aula 27/08/2026

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
+/* import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
@@ -17,4 +17,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+}); */
+import React from "react";
+import { Home } from "./src/screens";
+
+export default function App() {
+ return <Home />;
+}
